b8e16b3b66a38d57840146349556e830802e74f2
Augusto Lima Camarano <69219821+alcamarano@users.noreply.github.com>
2026-04-12 11:37:22 -0300

    feat: 1003 - Soma Simples

---
+++ iniciante/1003.py
@@ -0,0 +1,5 @@
+# Ler dois valores e armazenar nas variáveis A e B
+A = int(input("Informe o primeiro número: "))
+B = int(input("Informe o segundo número: "))
+# Exibir a soma entre A e B
+print("SOMA =", A + B)
