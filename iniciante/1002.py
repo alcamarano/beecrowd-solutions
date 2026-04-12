f840ab98c73c19e5c8a5e8d50d8248392e870df2
Augusto Lima Camarano <69219821+alcamarano@users.noreply.github.com>
2026-04-12 11:35:42 -0300

    feat: 1002 - Área do Círculo

---
+++ iniciante/1002.py
@@ -0,0 +1,6 @@
+# Ler o valor do raio de um círculo e armazenar na variável raio
+raio = float(input("Informe o raio do círculo: "))
+# Calcular o valor da área do círculo
+area = 3.14159 * raio ** 2
+# Exibir a area do círculo
+print("A={:.4f}".format(area))
