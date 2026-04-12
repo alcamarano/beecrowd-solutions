41cc22caeb3e38af6581bf1d7e75e68de32f033a
Augusto Lima Camarano <69219821+alcamarano@users.noreply.github.com>
2026-04-12 12:29:01 -0300

    feat: 1016 - Distância

---
+++ iniciante/1016.py
@@ -0,0 +1,6 @@
+# Ler a distância em km e armazenar na variável distância
+distancia = int(input("Informe a distância: "))
+# Realizar o cálculo de quantos minutos um carro se afasta do outro conforme a distância
+minutos = distancia * 2
+# Exibir o resultado
+print(f"{minutos} minutos")
