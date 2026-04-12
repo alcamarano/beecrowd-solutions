19c07f1f69be5a4b4ee87f36c728ae41a4735b48
Augusto Lima Camarano <69219821+alcamarano@users.noreply.github.com>
2026-04-12 11:32:36 -0300

    feat: 1001.py - Extremamente Básico

---
+++ iniciante/1001.py
@@ -0,0 +1,7 @@
+# Ler dois valores e armazenar nas variáveis A e B
+A = int(input("Informe o primeiro número: "))
+B = int(input("Informe o segundo número: "))
+# Realizar a soma entra A e B
+X = A + B
+# Exibir o resultado
+print("X = {}".format(X))
