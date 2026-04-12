4941df8891a4eb0e29be97f22aca77c71fd55ab2
Augusto Lima Camarano <69219821+alcamarano@users.noreply.github.com>
2026-04-12 12:02:23 -0300

    feat: 1011 - Esfera

---
+++ iniciante/1011.py
@@ -0,0 +1,6 @@
+# Ler o valor do raio e armazenar na variável raio
+raio = float(input())
+# Realizar o cálculo do volume de uma esfera e armazenar na variável volume
+volume = (4.0/3) * 3.14159 * raio ** 3
+# Exibir o resultado
+print("VOLUME = {:.3f}".format(volume))
