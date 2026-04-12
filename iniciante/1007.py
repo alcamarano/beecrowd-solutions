e89f8095cb50eed506cbb5e1cdd5e45a34ad3e52
Augusto Lima Camarano <69219821+alcamarano@users.noreply.github.com>
2026-04-12 11:46:51 -0300

    feat: 1007 - Diferença

---
+++ iniciante/1007.py
@@ -0,0 +1,9 @@
+# Ler quatro valores e armazenar nas variáveis A, B, C e D
+A = int(input("Informe o primeiro valor: "))
+B = int(input("Informe o segundo valor: "))
+C = int(input("Informe o terceiro valor: "))
+D = int(input("Informe o quarto valor: "))
+# Realizar a diferença entre o produto de A e B, e de C e D
+DIFERENCA = (A * B - C * D)
+# Exibir o resultado
+print("DIFERENCA =", DIFERENCA)
