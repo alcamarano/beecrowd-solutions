a5f072a1ebd1b39962e85a93c3a970276be17df9
Augusto Lima Camarano <69219821+alcamarano@users.noreply.github.com>
2026-04-12 11:39:07 -0300

    feat: 1004 - Produto Simples

---
+++ iniciante/1004.py
@@ -0,0 +1,7 @@
+# Ler dois valores e armazenar nas variáveis A e B
+A = int(input("Informe o primeiro número: "))
+B = int(input("Informe o segundo número: "))
+# Realizar a multiplicação de A e B
+PROD = (A * B)
+# Exibir o resultado
+print("PROD =", PROD)
